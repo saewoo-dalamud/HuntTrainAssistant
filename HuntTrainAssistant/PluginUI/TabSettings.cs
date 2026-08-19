7201a751bd5d0b8df918ce88d8a0b33a70878a70
saewoo <saewoo.zip@proton.me>
2026-08-20 01:27:18 +0900

    설정 메뉴와 선호 탈것 UX 정리
    English: Refine the settings menu and preferred mount UX

--- HuntTrainAssistant/PluginUI/TabSettings.cs
+++ HuntTrainAssistant/PluginUI/TabSettings.cs
@@ -44,30 +44,52 @@ public unsafe class TabSettings
 								ImGui.Checkbox(Loc.Get("Settings.PluginEnabled"), ref P.Config.Enabled);
 								ImGui.SameLine();
 								ImGui.Checkbox(Loc.Get("Settings.DebugMode"), ref P.Config.Debug);
-								ImGui.Checkbox(Loc.Get("Settings.AutoTeleportDifferentZone"), ref P.Config.AutoTeleport);
-                ImGui.Indent();
-                ImGui.Checkbox(Loc.Get("Settings.AutoSwitchInstanceOne"), ref P.Config.AutoSwitchInstanceToOne);
-								ImGui.Checkbox(Loc.Get("Settings.MountAfterTeleport"), ref P.Config.UseMount);
-								if(P.Config.UseMount)
-								{
-										ImGui.Indent();
-                    ImGui.SetNextItemWidth(200f);
-                    ImGuiEx.Combo(Loc.Get("Settings.PreferredMount"), ref P.Config.Mount, Mounts.Keys, names: Mounts);
-                    ImGui.Unindent();
-								}
-                ImGui.Unindent();
                 ImGui.Checkbox(Loc.Get("Settings.AutoOpenMap"), ref P.Config.AutoOpenMap);
 								ImGui.Indent();
 								ImGui.Checkbox(Loc.Get("Settings.NoDuplicateFlags"), ref P.Config.NoDuplicateFlags);
 								ImGui.Unindent();
+								ImGui.Checkbox(Loc.Get("Settings.ContextMenu"), ref P.Config.ContextMenu);
+						})
+						.Section(Loc.Get("Settings.Sections.Movement"))
+						.Widget(() =>
+						{
+								ImGui.Checkbox(Loc.Get("Settings.AutoTeleportDifferentZone"), ref P.Config.AutoTeleport);
+								if(P.Config.AutoTeleport)
+								{
+										ImGui.Indent();
+										ImGui.Checkbox(Loc.Get("Settings.AutoSwitchInstanceOne"), ref P.Config.AutoSwitchInstanceToOne);
+										ImGui.Checkbox(Loc.Get("Settings.AutoNextInstance"), ref P.Config.AutoSwitchInstanceTwoRanks);
+										ImGui.Checkbox(Loc.Get("Settings.RandomTeleportDelay"), ref P.Config.TeleportDelayEnabled);
+										if(P.Config.TeleportDelayEnabled)
+										{
+												ImGui.Indent();
+												ImGui.SetNextItemWidth(150f);
+												ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.MinimumDelay"), ref P.Config.TeleportDelayMin, 0, 1000);
+												ImGui.SetNextItemWidth(150f);
+												ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.MaximumDelay"), ref P.Config.TeleportDelayMax, 0, 1000);
+												ImGui.Unindent();
+										}
+										ImGui.Unindent();
+								}
+								ImGui.Checkbox(Loc.Get("Settings.MountAfterTeleport"), ref P.Config.UseMount);
+								ImGui.AlignTextToFramePadding();
+								ImGui.Text(Loc.Get("Settings.PreferredMount"));
+								ImGui.SameLine();
+								ImGui.SetNextItemWidth(200f);
+								ImGuiEx.Combo("##PreferredMount", ref P.Config.Mount, Mounts.Keys, names: Mounts);
 								ImGui.Checkbox(Loc.Get("Settings.AutoFlyAfterCombat"), ref P.Config.AutoFlyToConductorLocation);
+								ImGuiEx.PluginAvailabilityIndicator([new("vnavmesh")]);
 								if(P.Config.AutoFlyToConductorLocation)
 								{
 										ImGui.Indent();
 										ImGui.Checkbox(Loc.Get("Settings.AutoMountForFlight"), ref P.Config.AutoMountForAutoFly);
-										ImGuiEx.PluginAvailabilityIndicator([new("vnavmesh")]);
 										ImGui.Unindent();
 								}
+								ImGui.Checkbox(Loc.Get("Settings.AetheryteCompensation"), ref P.Config.DistanceCompensationHack);
+						})
+						.Section(Loc.Get("Settings.Sections.Conductors"))
+						.Widget(() =>
+						{
 								ImGui.Checkbox(Loc.Get("Settings.SuppressOthers"), ref P.Config.SuppressChatOtherPlayers);
 								var keepConductors = !P.Config.ClearConductorsOutsideHuntingTerritory;
 								if(ImGui.Checkbox(Loc.Get("Settings.KeepConductorsOutsideTerritory"), ref keepConductors))
@@ -82,27 +104,24 @@ public unsafe class TabSettings
 										{
 												ImGui.Indent();
 												ImGui.SetNextItemWidth(150f);
-												ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.ConductorTimeoutMinutes"), ref P.Config.ConductorInactivityTimeoutMinutes, 5, 120);
+												ImGui.SliderInt(Loc.Get("Settings.ConductorTimeoutMinutes"), ref P.Config.ConductorInactivityTimeoutMinutes, 5, 120);
 												ImGui.Unindent();
 										}
 										ImGui.Unindent();
 								}
-								ImGui.Checkbox(Loc.Get("Settings.AetheryteCompensation"), ref P.Config.DistanceCompensationHack);
-								ImGui.Checkbox(Loc.Get("Settings.AutoNextInstance"), ref P.Config.AutoSwitchInstanceTwoRanks);
-								ImGui.Checkbox(Loc.Get("Settings.ContextMenu"), ref P.Config.ContextMenu);
+						})
+						.Section(Loc.Get("Settings.Sections.PartyFinder"))
+						.Widget(() =>
+						{
 								ImGui.Checkbox(Loc.Get("Settings.EnablePartyFinderButton"), ref P.Config.PfinderEnable);
-								ImGui.Indent();
-								ImGuiEx.Text(Loc.Get("Settings.PartyFinderComment"));
-								ImGuiEx.SetNextItemFullWidth();
-								ImGui.InputText($"##pfindercommenr", ref P.Config.PfinderString, 150);
-								ImGui.Unindent();
-								ImGui.Checkbox(Loc.Get("Settings.RandomTeleportDelay"), ref P.Config.TeleportDelayEnabled);
-								ImGui.Indent();
-								ImGui.SetNextItemWidth(150f);
-                ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.MinimumDelay"), ref P.Config.TeleportDelayMin, 0, 1000);
-                ImGui.SetNextItemWidth(150f);
-                ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.MaximumDelay"), ref P.Config.TeleportDelayMax, 0, 1000);
-                ImGui.Unindent();
+								if(P.Config.PfinderEnable)
+								{
+										ImGui.Indent();
+										ImGuiEx.Text(Loc.Get("Settings.PartyFinderComment"));
+										ImGuiEx.SetNextItemFullWidth();
+										ImGui.InputText($"##pfindercommenr", ref P.Config.PfinderString, 150);
+										ImGui.Unindent();
+								}
 						})
 						.Section(Loc.Get("Settings.Sections.Notifications"))
 						.Widget(() =>
@@ -110,28 +129,31 @@ public unsafe class TabSettings
 								ImGuiEx.Text(Loc.Get("Settings.NotificationMasterRequired"));
 								ImGuiEx.PluginAvailabilityIndicator([new("NotificationMaster"), new("NotificationMaster.NXIV", "NotificationMaster (from NightmareXIV repo)")], "", false);
 								ImGui.Checkbox(Loc.Get("Settings.PlayConductorAudio"), ref P.Config.AudioAlert);
-								ImGui.Indent();
-								ImGuiEx.InputWithRightButtonsArea(() => ImGui.InputTextWithHint("##pathToAudio", Loc.Get("Settings.AudioFilePath"), ref P.Config.AudioAlertPath, 500), () =>
+								if(P.Config.AudioAlert)
 								{
-										if(ImGui.Button(Loc.Get("Common.Select")))
+										ImGui.Indent();
+										ImGuiEx.InputWithRightButtonsArea(() => ImGui.InputTextWithHint("##pathToAudio", Loc.Get("Settings.AudioFilePath"), ref P.Config.AudioAlertPath, 500), () =>
 										{
-												OpenFileDialog.SelectFile((x) =>
+												if(ImGui.Button(Loc.Get("Common.Select")))
 												{
-														if(x != null) new TickScheduler(() => P.Config.AudioAlertPath = x.file);
-												});
-										}
-										ImGui.SameLine();
-										if(ImGuiEx.IconButton(FontAwesomeIcon.Play))
-										{
-												S.Notificator.PlaySound(P.Config.AudioAlertPath, P.Config.AudioAlertVolume, false, false);
-										}
-								});
-								ImGui.SetNextItemWidth(150f);
-								ImGui.SliderFloat(Loc.Get("Settings.Volume"), ref P.Config.AudioAlertVolume, 0f, 1f);
-								ImGui.Checkbox(Loc.Get("Settings.AudioOnlyMinimized"), ref P.Config.AudioAlertOnlyMinimized);
-                ImGui.SetNextItemWidth(150f);
-                ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.AudioMinimumInterval"), ref P.Config.AudioThrottle, 0, 10000);
-                ImGui.Unindent();
+														OpenFileDialog.SelectFile((x) =>
+														{
+																if(x != null) new TickScheduler(() => P.Config.AudioAlertPath = x.file);
+														});
+												}
+												ImGui.SameLine();
+												if(ImGuiEx.IconButton(FontAwesomeIcon.Play))
+												{
+														S.Notificator.PlaySound(P.Config.AudioAlertPath, P.Config.AudioAlertVolume, false, false);
+												}
+										});
+										ImGui.SetNextItemWidth(150f);
+										ImGui.SliderFloat(Loc.Get("Settings.Volume"), ref P.Config.AudioAlertVolume, 0f, 1f);
+										ImGui.Checkbox(Loc.Get("Settings.AudioOnlyMinimized"), ref P.Config.AudioAlertOnlyMinimized);
+										ImGui.SetNextItemWidth(150f);
+										ImGuiEx.SliderIntAsFloat(Loc.Get("Settings.AudioMinimumInterval"), ref P.Config.AudioThrottle, 0, 10000);
+										ImGui.Unindent();
+								}
                 ImGui.Checkbox(Loc.Get("Settings.FlashTaskbar"), ref P.Config.FlashTaskbar);
 								ImGui.Checkbox(Loc.Get("Settings.TrayNotification"), ref P.Config.TrayNotification);
             })
